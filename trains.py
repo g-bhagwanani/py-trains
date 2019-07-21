3fa8277930e97ad8611a43ae86028c1ee07907e3
Gaurav Bhagwanani <gaurav.bhagwanani@gmail.com>
2019-07-21 14:31:57 +0530

    First commit

---
+++ trains.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+#change the following 5 variables as per the requirement
+source_stn = 'KYN'
+dest_stn = 'NZM'
+date = '25'
+month = '10'
+year = '2019'
+
+url = 'https://www.railyatri.in/booking/trains-between-stations?from_code='+source_stn+'&from_name=SRC+&journey_date='+date+'%2F'+month+'%2F'+year+'%2F'+'&to_code='+dest_stn+'&to_name=DST+&user_id=1&user_token=6'
+
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+trains = []
+for row in soup.select('tr.tbs-main-row'):
+	data = {}
+	title = row.find('p', {'class': 'train-name-title'}).text.strip('\n')
+	data['trn_no'] = title[:5]
+	data['trn_name'] = title[8:]
+	data['schedule'] = 'https://erail.in/train-enquiry/'+title[:5]
+	spans = row.find_all('span')[:6]
+	data['source'] = spans[0].text
+	data['departure time'] = spans[1].text
+	data['destination'] = spans[3].text
+	data['arrival time'] = spans[4].text
+	data['duration'] = spans[5].text
+	data['fares'] = []
+	classes = row.find_all('div', {'class': 'coach'})
+	for i in range(len(classes)//2):
+		pair = {}
+		deets = classes[i].find_all('p')
+		pair['Class'] = deets[0].text
+		pair['Price'] = deets[1].text
+		if pair['Price'] == '₹ NA':
+			continue
+		data['fares'].append(pair)
+	if len(data['fares']) != 0:		#railyatri shows some trains with no availabilities
+		trains.append(data)
+
+pprint(trains)
